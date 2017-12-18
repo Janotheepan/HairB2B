98c5365408f0b635c6aca4f3e10f0a3830bd7ee4
Mariyathas Janotheepan <mariyathas_j@3logixs.lk>
2017-12-18 15:07:25 +0530

    third commit

--- src/app/stylist/stylist.component.ts
+++ src/app/stylist/stylist.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+// import {AppStore} from './app.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-stylist',
@@ -6,18 +8,29 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./stylist.component.css']
 })
 export class StylistComponent implements OnInit {
-  stylist = {
-    profile_pic : '../images/stylist/jj.jpg',
-    name : 'Jano',
-    star : '*****',
-    skills : 'Colouring',
-    description : 'I can work well and colour perfectly.',
-    payment : '$250'
-  };
+   stylist = {
+     profile_pic: './images/stylist/jj.jpg',
+     name: 'Jano',
+     star: '*****',
+     skills: 'Colouring',
+     description: 'I can work well and colour perfectly.',
+     payment: '$250'
+   };
+   public date = moment();
+
+   public daysArr;
 
   constructor() {
   }
 
   ngOnInit() {
+    this.daysArr = this.createCalendar(this.date);
+  }
+
+  createCalendar(month) {
+    const firstDay = moment(month).startOf('M');
+    const days = Array.apply(null, {length: month.daysInMonths()})
+      .map(Number.call, Number);
+    return days;
   }
 }
